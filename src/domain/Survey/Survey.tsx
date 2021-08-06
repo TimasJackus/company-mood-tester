9c470705e8a7d3c2d098084d1c79dded63efa941
Timas Jackus <jackustimas@gmail.com>
2021-08-07 02:13:42 +0300

    contact hr alert

--- src/domain/Survey/Survey.tsx
+++ src/domain/Survey/Survey.tsx
@@ -3,7 +3,9 @@ import { Card } from "../../ui/Card/Card";
 import { Row } from "../../ui/Row/Row";
 import { Column } from "../../ui/Column/Column";
 import { Span } from "../../ui/Span/Span";
+import { Alert } from "../../ui/Alert/Alert";
 import { Divider } from "../../ui/Divider";
+import { Link } from "../../ui/Link/Link";
 import Rating from "react-rating";
 import {
   AiFillStar,
@@ -42,44 +44,54 @@ const Survey: React.FC = () => {
   })();
 
   return (
-    <Card center spaceBetween>
-      {rating ? (
-        <>
-          <Column center>
-            <Span size={24}>
-              Company Mood <Span weight={700}>Today</Span>
-            </Span>
-            <Row centerH center mt={10}>
-              <FaceIcon size={48} color={getColorByRating()} />{" "}
-              <Span color={getColorByRating()} size={24} weight={500} ml={10}>
-                {averageRating}
-              </Span>
-            </Row>
-          </Column>
-          <Divider />
-        </>
-      ) : (
-        <>
-          <Row spaceBetween center>
-            <Span size={16} weight={700}>
-              How was your day?
-            </Span>
-            <Rating
-              onChange={onChange}
-              emptySymbol={<AiOutlineStar size={28} color="#ffc000" />}
-              fullSymbol={<AiFillStar size={28} color="#ffc000" />}
-            />
-          </Row>
-          <Divider />
-        </>
+    <>
+      {rating && rating <= 2 && (
+        <Alert>
+          <Span weight={700} size={16}>
+            Some text for help...
+          </Span>
+          <Link href="mailto:hr@mediapark.com">Contact</Link>
+        </Alert>
       )}
-      <Row>
-        <Span color="#80a0a9">
-          Mood test streak: <Span weight={700}>1</Span> <br />
-          Personal record: <Span weight={700}>3</Span>
-        </Span>
-      </Row>
-    </Card>
+      <Card center spaceBetween>
+        {rating ? (
+          <>
+            <Column center>
+              <Span size={24}>
+                Company Mood <Span weight={700}>Today</Span>
+              </Span>
+              <Row centerH center mt={10}>
+                <FaceIcon size={48} color={getColorByRating()} />{" "}
+                <Span color={getColorByRating()} size={24} weight={500} ml={10}>
+                  {averageRating}
+                </Span>
+              </Row>
+            </Column>
+            <Divider />
+          </>
+        ) : (
+          <>
+            <Row spaceBetween center>
+              <Span size={16} weight={700}>
+                How was your day?
+              </Span>
+              <Rating
+                onChange={onChange}
+                emptySymbol={<AiOutlineStar size={28} color="#ffc000" />}
+                fullSymbol={<AiFillStar size={28} color="#ffc000" />}
+              />
+            </Row>
+            <Divider />
+          </>
+        )}
+        <Row>
+          <Span color="#80a0a9">
+            Mood test streak: <Span weight={700}>1</Span> <br />
+            Personal record: <Span weight={700}>3</Span>
+          </Span>
+        </Row>
+      </Card>
+    </>
   );
 };
 
---
+++ src/ui/Alert/Alert.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Alert = styled.div`
+  background: #ffa100;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  color: #fff;
+  padding: 10px 20px;
+`;
