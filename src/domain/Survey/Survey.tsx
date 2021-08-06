6f7cea36ad12ae99be068b778a792724e2a5d86c
Karolis Vilpisauskas <karolis.vilpisauskas@mediapark.com>
2021-08-06 22:25:13 +0300

    fix: Card imports

--- src/domain/Survey/Survey.tsx
+++ src/domain/Survey/Survey.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "../../ui/Card";
+import { Card } from "../../ui/ Card/Card";
 
 const Survey: React.FC = () => {
   return <Card>How was your day?</Card>;
