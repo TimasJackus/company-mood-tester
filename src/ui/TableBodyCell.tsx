ef0ba4b0ef3194551d1f620cba5f2074f6653db4
Timas Jackus <jackustimas@gmail.com>
2021-08-07 17:14:02 +0300

    fix

--- src/ui/TableBodyCell.tsx
+++ src/ui/TableBodyCell.tsx
@@ -1,16 +1,15 @@
 import styled from "styled-components";
 
 export const TableBodyCell = styled.td`
-    width: 26px;
-    height: 31px;
-    line-height: 28px;
-    overflow: hidden;
-    border-bottom: 1px solid #ccdedf;
-    border-left: 1px solid #ccdedf;
-    text-align: center;
-    position: relative;
-    font-size: 11px;
-    box-sizing: border-box;
-    top: -1px;
-    padding-top: 3px;
-`
+  width: 26px;
+  height: 31px;
+  line-height: 28px;
+  overflow: hidden;
+  border-bottom: 1px solid #ccdedf;
+  border-left: 1px solid #ccdedf;
+  text-align: center;
+  position: relative;
+  font-size: 11px;
+  box-sizing: border-box;
+  padding-top: 3px;
+`;
