266946bbbc96c8cd9227aef1b0ae0584428594b8
Karolis Vilpisauskas <karolis.vilpisauskas@mediapark.com>
2021-08-07 14:30:06 +0300

    Added useSWR config

--- src/application/App.tsx
+++ src/application/App.tsx
@@ -1,9 +1,11 @@
+import { SWRConfig } from 'swr';
+import { swrConfig } from '../infrastructure/api';
 import Router from './Router';
 
 function App() {
   return (
     <div id="root">
-      <Router />
+      <SWRConfig value={swrConfig}><Router /></SWRConfig>
     </div>
   );
 }
--- src/infrastructure/api.ts
+++ src/infrastructure/api.ts
@@ -9,7 +9,7 @@ const defaultOptions = {
 };
 
 // Create instance
-const instance = axios.create(defaultOptions);
+export const instance = axios.create(defaultOptions);
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uSWQiOjgsInVzZXJJZCI6InFVbDJkVG1HckgwSm4za0VvOUlMRyIsInR5cGUiOiJzZXNzaW9uIiwiaWF0IjoxNjI4MzM1NDYyfQ.H8LCJXRjjUGFdtvTl09FUpNpYNvq_f-5VX2cyJc09NE"
 
@@ -18,3 +18,9 @@ instance.interceptors.request.use(function (config) {
     config.headers.Authorization =  token ? `Bearer ${token}` : '';
     return config;
 });
+
+export const fetcher = (url: string) => instance.get(url).then(res => res.data)
+
+export const swrConfig = {
+    fetcher,
+}
