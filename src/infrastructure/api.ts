bed2f16176c8bc6b76b96b552b0bbbb148352a00
Karolis Vilpisauskas <karolis.vilpisauskas@mediapark.com>
2021-08-07 14:25:42 +0300

    Added packages and axios instance

---
+++ src/infrastructure/api.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+// Default config options
+const defaultOptions = {
+    baseURL: "http://192.168.8.152:3000",
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
+
+// Create instance
+const instance = axios.create(defaultOptions);
+
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uSWQiOjgsInVzZXJJZCI6InFVbDJkVG1HckgwSm4za0VvOUlMRyIsInR5cGUiOiJzZXNzaW9uIiwiaWF0IjoxNjI4MzM1NDYyfQ.H8LCJXRjjUGFdtvTl09FUpNpYNvq_f-5VX2cyJc09NE"
+
+// Set the AUTH token for any request
+instance.interceptors.request.use(function (config) {
+    config.headers.Authorization =  token ? `Bearer ${token}` : '';
+    return config;
+});
